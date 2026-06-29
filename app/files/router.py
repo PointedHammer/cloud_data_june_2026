d1ed33b93b78b620f2dda565cafa1600821f8806
Kku3t3 <javiergiral7@gmail.com>
2026-06-29 18:00:15 +0200

    Base content

--- app/files/router.py
+++ app/files/router.py
@@ -1,11 +1,7 @@
-from email.header import Header
-import httpx
-import token
 from typing import Optional
-import hashlib
-import uuid
 from fastapi import APIRouter, Body, Header, HTTPException, status  # type: ignore
 from pydantic import BaseModel # type: ignore
+import httpx
 
 router = APIRouter()
 
@@ -15,53 +11,48 @@ class User(BaseModel):
 
 async def introspect(token: str) -> User:
     url = "0.0.0.0:80/introspect"
-
     headers = {
-
         "accept": "application/json",
-
         "auth": token
-
     }
-    async def introspect(token: str) -> User:
-        async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
 
-            response = await client.get(url, headers=headers)
+    print(response)
 
-        print(response)
-
-        if response.status_code != 200:
-
-            raise HTTPException(
-
-            status_code=401, detail="Unauthorized"
-
-        )
+    if response.status_code != 200:
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
     return User(**response.json())
 
 @router.get("/files")
 async def Get(Auth: str = Header()) -> dict[str, str]:
+    currrent_user = await introspect(Auth)
     return {"status": "ok"}
 
 @router.post("/files")
 async def post(Auth: str = Header()) -> dict[str, str]:
+    current_user = await introspect(Auth)
     return {"status": "ok"}
 
 @router.post("/merge")
 async def merge_post(Auth: str = Header()) -> dict[str, str]:
+    current_user = await introspect(Auth)
     return {"status": "ok"}
 
 @router.get("/files/{id}")
 async def get_by_id(id: int, Auth: str = Header()) -> dict[str, str]:
+    current_user = await introspect(Auth)
     return {"status": "ok"}
 
 @router.post("/files/{id}")
 async def post_id(id: int, Auth: str = Header()) -> dict[str, str]:
+    current_user = await introspect(Auth)
     return {"status": "ok"}
 
 @router.delete("/files/{id}")
 async def delete_id(id: int, Auth: str = Header()) -> dict[str, str]:
+    current_user = await introspect(Auth)
     return {"status": "ok"}
 
 
